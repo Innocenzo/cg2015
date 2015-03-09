72e04d59447b2a273cc9162a501c5d98647affa0
Innocenzo Biondo <innocenzobiondo@gmail.com>
2015-03-09 14:19:15 +0100

    Esercizio 1 3

---
+++ esercizio1 (copia).js
@@ -0,0 +1,37 @@
+var vet= new Array(10);
+for (var i = 0; i < vet.length; i++) {
+	vet[i]=new Array(10);
+}
+
+for (var i = 0; i < vet.length; i++) {
+	for (var j = 0; j < vet.length; j++){
+	if (i!=0) {
+		vet[i][j]=vet[i-1][j]+vet[0][j];
+	} else{
+		vet[i][j]=j+1;
+	} 
+		
+	}
+	
+}
+for (var i = 0; i < vet.length; i++) {
+	console.log(vet[i]);	
+	document.write(vet[i]);
+	document.write('\n');
+};
+
+for (var i = 0; i < vet.length; i++) {
+	for (var j = 0; j < vet.length; j++){
+		if (i==j) {
+			vet[i][j]=1;
+		}else{ 	
+			vet[i][j]=0;
+			}
+		}
+	
+}
+for (var i = 0; i < vet.length; i++) {
+	console.log(vet[i]);	
+	document.write(vet[i]);
+	document.write('\n');
+};
