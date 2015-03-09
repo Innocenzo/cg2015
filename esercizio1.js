def16dec0a66b046c10d78a682d90de895e25c6c
Innocenzo Biondo <innocenzobiondo@gmail.com>
2015-03-09 14:43:11 +0100

    Esercizio 2

--- esercizio1.js
+++ esercizio1.js
@@ -19,6 +19,28 @@ for (var i = 0; i < vet.length; i++) {
 	document.write(vet[i]);
 	document.write('\n');
 };
+for (var i = 0; i < vet.length; i++) {
+	for (var j = 0; j < vet.length; j++){
+	if (i!=0) {
+		vet[i][j]=vet[i-1][j]+vet[0][j];
+	} else{
+		vet[i][j]=j+1;
+	} 
+		
+	}
+}
+for (var i = 0; i < vet.length; i++) {
+	for (var j = 0; j < vet.length-1; j++){
+		
+		vet[i][j]+=',';
+	}
+	
+}
+for (var i = 0; i < vet.length; i++) {
+	console.log(vet[i]);	
+	document.write(vet[i]);
+	document.write('\n');
+};
 
 for (var i = 0; i < vet.length; i++) {
 	for (var j = 0; j < vet.length; j++){
@@ -34,4 +56,5 @@ for (var i = 0; i < vet.length; i++) {
 	console.log(vet[i]);	
 	document.write(vet[i]);
 	document.write('\n');
+	//prova
 };
